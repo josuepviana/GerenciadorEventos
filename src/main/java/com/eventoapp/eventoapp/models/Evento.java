d8f89d01ae4f162fbf64f9862e1c1d5569002c0a
Josue Viana <josuep.viana@gmail.com>
2022-06-19 20:25:54 -0300

    first commit

---
+++ src/main/java/com/eventoapp/eventoapp/models/Evento.java
@@ -0,0 +1,80 @@
+package com.eventoapp.eventoapp.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+public class Evento implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long codigo;
+
+    private String nome;
+    private String ministrador;
+    private String local;
+    private String data;
+    private String horarioInicio;
+    private String horarioFim;
+
+    public long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(long codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMinistrador() {
+        return ministrador;
+    }
+
+    public void setMinistrador(String ministrador) {
+        this.ministrador = ministrador;
+    }
+
+    public String getLocal() {
+        return local;
+    }
+
+    public void setLocal(String local) {
+        this.local = local;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getHorarioInicio() {
+        return horarioInicio;
+    }
+
+    public void setHorarioInicio(String horarioInicio) {
+        this.horarioInicio = horarioInicio;
+    }
+
+    public String getHorarioFim() {
+        return horarioFim;
+    }
+
+    public void setHorarioFim(String horarioFim) {
+        this.horarioFim = horarioFim;
+    }
+}
---
+++ src/main/java/com/eventoapp/eventoapp/repository/EventoRepository.java
@@ -0,0 +1,10 @@
+package com.eventoapp.eventoapp.repository;
+
+import com.eventoapp.eventoapp.models.Evento;
+import org.springframework.data.repository.CrudRepository;
+
+public interface EventoRepository extends CrudRepository<Evento, String> {
+
+    Evento findByCodigo(long codigo);
+
+}
