6150ece392d8241ba23b8f2270ac0d901e12b533
Josue Viana <josuep.viana@gmail.com>
2023-02-25 18:38:01 -0300

    Ajustes

---
+++ src/main/java/com/eventoapp/eventoapp/ConfigureEventoappApplication.java
@@ -0,0 +1,8 @@
+package com.eventoapp.eventoapp;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+public interface ConfigureEventoappApplication {
+
+    SpringApplicationBuilder configure(SpringApplicationBuilder application);
+}
--- src/main/java/com/eventoapp/eventoapp/EventoappApplication.java
+++ src/main/java/com/eventoapp/eventoapp/EventoappApplication.java
@@ -2,12 +2,17 @@ package com.eventoapp.eventoapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class EventoappApplication {
+public class EventoappApplication implements ConfigureEventoappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EventoappApplication.class, args);
 	}
 
+	@Override
+	public SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(EventoappApplication.class);
+	}
 }
